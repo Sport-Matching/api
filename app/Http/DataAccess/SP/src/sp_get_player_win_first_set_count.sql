b1b4e8331a51b6f9aa1d91721c69a9bac3be2c5a
Robin THONI <robin@rthoni.com>
2016-05-24 15:36:06 +0200

    fixed division by zero

--- app/Http/DataAccess/SP/src/sp_get_player_win_first_set_count.sql
+++ app/Http/DataAccess/SP/src/sp_get_player_win_first_set_count.sql
@@ -1,4 +1,10 @@
 
-        SELECT count(*)::float / (SELECT count(*) FROM sp_get_matches_by_player(player_id))::float * 100.0 FROM matches m
+        SELECT count(*)::float /
+                (CASE (SELECT count(*) FROM sp_get_matches_by_player(player_id))::float
+                WHEN 0 THEN 1
+                ELSE (SELECT count(*) FROM sp_get_matches_by_player(player_id))::float
+                END
+                )
+                * 100.0 FROM matches m
         LEFT JOIN sets s ON s.id = m.set1_id
         WHERE m.player1_id = player_id AND sp_get_set_winner(s.id) = 1
--- sql/struct_init.sql
+++ sql/struct_init.sql
@@ -302,9 +302,15 @@ $$;
 CREATE FUNCTION sp_get_player_win_first_set_count(player_id integer, OUT percent double precision) RETURNS double precision
     LANGUAGE sql
     AS $$
-        SELECT count(*)::float / (SELECT count(*) FROM sp_get_matches_by_player(player_id))::float * 100.0 FROM matches m
+    SELECT count(*)::float /
+           (CASE (SELECT count(*) FROM sp_get_matches_by_player(player_id))::float
+            WHEN 0 THEN 1
+            ELSE (SELECT count(*) FROM sp_get_matches_by_player(player_id))::float
+            END
+           )
+           * 100.0 FROM matches m
         LEFT JOIN sets s ON s.id = m.set1_id
-        WHERE m.player1_id = player_id AND sp_get_set_winner(s.id) = 1
+    WHERE m.player1_id = player_id AND sp_get_set_winner(s.id) = 1
 $$;
 
 
